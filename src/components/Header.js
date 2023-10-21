8c68d9ce5766b6d4939e53170cae951adfb9cc4b
Carlos A. Estrada <cestr008@gmail.com>
2023-10-20 23:58:46 -0400

    complete step 5

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,6 +33,37 @@ const socials = [
 ];
 
 const Header = () => {
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+
+    const handleScroll = () => {
+      const currScrollPos = window.scrollY;
+      const currHeaderElement = headerRef.current;
+
+      if (!currHeaderElement){
+        return;
+      }
+        
+    
+      if (prevScrollPos > currScrollPos) {
+        currHeaderElement.style.transform = "translateY(0)";
+      } else {
+        currHeaderElement.style.transform = "translateY(-200px)";
+      }
+      
+      prevScrollPos = currScrollPos;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
@@ -55,6 +86,7 @@ const Header = () => {
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
       backgroundColor="#18181b"
+      ref={headerRef}
     >
       <Box color="white" maxWidth="1280px" margin="0 auto">
         <HStack
